7e4bbc5fa472c982c25bfa981054649058315266
Saikat Sikder <45892103+mdhsaikats@users.noreply.github.com>
2024-02-13 10:27:12 +0600

    new_code_lab_class_struct

---
+++ doubly_link_list.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *pre;
+    struct node *next;
+};
+
+int main()
+{
+    struct node *head, *second , *third ,*ptr;
+    head=malloc(sizeof(struct node));
+    second=malloc(sizeof(struct node));
+    third=malloc(sizeof(struct node));
+
+    head->data=6;
+    head->next=second;
+    head->pre=NULL;
+
+    second->data=7;
+    second->next=third;
+    second->pre=head;
+
+    third->data=8;
+    third->next=NULL;
+    third->pre=second;
+
+    free(third);
+    third=NULL;
+
+    ptr=head;
+    while( ptr != NULL)
+    {
+        printf("%d\n",ptr->data);
+        ptr=ptr->next;
+    }
+
+    return 0;
+
+
+
+}
+
+
+
+
---
+++ node.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+ struct node
+ {
+     int data;
+     struct node *next;
+ };
+ int main()
+ {
+     struct node *a=malloc(sizeof(struct node)) , *b , *c , *d , *ptr , *temp, *del1,*del2 ,*delpre, *delcur;
+     b=malloc(sizeof(struct node));
+     c=malloc(sizeof(struct node));
+     d=malloc(sizeof(struct node));
+     a->data=7;
+     a->next=b;
+     b->data=8;
+     b->next=c;
+     c->data=9;
+     c->next=d;
+     d->data=10;
+     d->next=NULL;
+
+     /*
+     temp = a;
+     a = a->next;
+     free(temp);
+     temp=NULL;
+     del1=a;
+     del2=a;
+
+     */
+
+     delpre=a;
+     delcur=a;
+     int delpos = ;
+
+     while(delpos != 1)
+     {
+         delpre = delcur;
+         delcur= delcur->next;
+         delpos--;
+     }
+
+     delpre->next=delcur->next;
+     free(delcur);
+     delcur=NULL;
+
+    /*while(del2->next != NULL)
+    {
+        del1=del2;
+        del2=del2->next;
+    }
+    del1->next=NULL;
+    free(del2);
+    del2=NULL;
+    */
+     ptr=a;
+     while(ptr != NULL)
+     {
+         printf("%d\n",ptr->data);
+         ptr=ptr->next;
+     }
+     return 0;
+
+
+
+
+
+
+ }
