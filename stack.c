a8189ee313028a72e571fca25006aab62ad7cefe
Saikat Sikder <45892103+mdhsaikats@users.noreply.github.com>
2024-02-18 22:59:54 +0600

    stack_file

---
+++ stack.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int s_array[5];
+int top=-1;
+
+
+void push(int data)
+{
+    top=top+1;
+    s_array[top]=data;
+}
+
+void pop()
+{
+    top--;
+}
+
+int main(){
+    push(1);
+    push(2);
+    push(3);
+
+    for(int i=top; i>=0; i--)
+    {
+        printf("%d\n",s_array[i]);
+    }
+}
